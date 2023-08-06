f31ab1f6210729b2416c13dad9a51d4de301b348
Restyled.io <commits@restyled.io>
2023-08-06 13:11:47 +0000

    Restyled by clang-format

--- server.js
+++ server.js
@@ -10,23 +10,19 @@ const bcrypt = require("bcrypt");
 require("dotenv").config();
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(intial_path));
 
 // Connecting the database
 const mongoose = require("mongoose");
 const fs = require("fs");
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => { console.log("Connected to MongoDB"); })
+    .catch((err) => { console.error("Error connecting to MongoDB:", err); });
 
-// Function to insert data from the JSON file into the database (Will run only at the time of database setup)
+// Function to insert data from the JSON file into the database (Will run only
+// at the time of database setup)
 async function insertDataToDB() {
   try {
     // Read the JSON file
@@ -42,16 +38,17 @@ async function insertDataToDB() {
 }
 
 // Call the function to insert data into the database
-// Uncomment it at first to insert all the data into database and then remove this call
-// and you can delete the function and youtubers.json file afterwards
+// Uncomment it at first to insert all the data into database and then remove
+// this call and you can delete the function and youtubers.json file afterwards
 // insertDataToDB();
 
 // Routes
 app.get("/", async (req, res) => {
   try {
-    const youtubers = await Youtuber.find({}); // Fetch all YouTubers from the database
+    const youtubers =
+        await Youtuber.find({}); // Fetch all YouTubers from the database
     return res.render("home", {
-      youtubers: youtubers, // Pass the fetched data to the EJS view
+      youtubers : youtubers, // Pass the fetched data to the EJS view
     });
   } catch (err) {
     console.error("Error fetching YouTubers:", err);
@@ -59,74 +56,67 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.get("/addYoutuber", (req, res) => {
-  res.render("addYoutuber.ejs", { errorMessage: "" });
-});
+app.get("/addYoutuber",
+        (req, res) => { res.render("addYoutuber.ejs", {errorMessage : ""}); });
 
-app.get("/removeYoutuber", (req, res) => {
-  res.render("removeYoutuber.ejs", { errorMessage: "" });
-});
+app.get(
+    "/removeYoutuber",
+    (req, res) => { res.render("removeYoutuber.ejs", {errorMessage : ""}); });
 
 app.post("/add", (req, res) => {
-  const { category, name, birthday, totalViews, link } = req.body;
-  Youtuber.findOne({ name: name })
-    .then((existingYoutuber) => {
-      if (existingYoutuber) {
-        // If the Youtuber with the same name already exists, show the error
-        const errorMessage = "Youtuber with this name already exists!";
-        return res.render("addYoutuber.ejs", { errorMessage });
-      } else {
-        // Create a new instance of the Youtuber model with the data
-        const newYoutuber = new Youtuber({
-          category,
-          name,
-          birthday,
-          totalViews,
-          link,
-        });
-
-        // Save the new YouTuber to the database
-        newYoutuber
-          .save()
-          .then(() => {
-            // Redirect back to the home page
-            return res.status(200).redirect("/");
-          })
-          .catch((err) => {
-            console.log("Error adding YouTuber:", err);
-            return res.status(500).send("Internal Server Error");
+  const {category, name, birthday, totalViews, link} = req.body;
+  Youtuber.findOne({name : name})
+      .then((existingYoutuber) => {
+        if (existingYoutuber) {
+          // If the Youtuber with the same name already exists, show the error
+          const errorMessage = "Youtuber with this name already exists!";
+          return res.render("addYoutuber.ejs", {errorMessage});
+        } else {
+          // Create a new instance of the Youtuber model with the data
+          const newYoutuber = new Youtuber({
+            category,
+            name,
+            birthday,
+            totalViews,
+            link,
           });
-      }
-    })
-    .catch((err) => {
-      console.log("Error checking for duplicate name:", err);
-      return res.status(500).send("Internal Server Error");
-    });
+
+          // Save the new YouTuber to the database
+          newYoutuber.save()
+              .then(() => {
+                // Redirect back to the home page
+                return res.status(200).redirect("/");
+              })
+              .catch((err) => {
+                console.log("Error adding YouTuber:", err);
+                return res.status(500).send("Internal Server Error");
+              });
+        }
+      })
+      .catch((err) => {
+        console.log("Error checking for duplicate name:", err);
+        return res.status(500).send("Internal Server Error");
+      });
 });
 
 app.post("/remove", (req, res) => {
-  const { key, name } = req.body;
+  const {key, name} = req.body;
 
-  bcrypt
-    .compare(key, process.env.SECRET_KEY)
-    .then((isMatch) => {
-      // If the key matches delete the youtuber
-      if (isMatch) {
-        Youtuber.findOneAndDelete({ name: name })
-          .then(() => {
-            return res.status(200).redirect("/");
-          })
-          .catch(() => res.status(400).json("Error!"));
-      } else {
-        // If the key doesn't match, set the errorMessage
-        const errorMessage = "Key does not match!";
-        return res.render("removeYoutuber.ejs", { errorMessage });
-      }
-    })
-    .catch((err) => console.log("Error comparing key", err));
+  bcrypt.compare(key, process.env.SECRET_KEY)
+      .then((isMatch) => {
+        // If the key matches delete the youtuber
+        if (isMatch) {
+          Youtuber.findOneAndDelete({name : name})
+              .then(() => { return res.status(200).redirect("/"); })
+              .catch(() => res.status(400).json("Error!"));
+        } else {
+          // If the key doesn't match, set the errorMessage
+          const errorMessage = "Key does not match!";
+          return res.render("removeYoutuber.ejs", {errorMessage});
+        }
+      })
+      .catch((err) => console.log("Error comparing key", err));
 });
 
 // Listening on port
-app.listen(port, () => {
-  console.log("Listening at port", port);
-});
+app.listen(port, () => { console.log("Listening at port", port); });
